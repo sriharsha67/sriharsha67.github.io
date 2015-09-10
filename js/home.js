7c8ccd0408fb606a5944c6ebdc9ed5ea319d255a
sriharsha67 <sriharsha67@gmail.com>
2015-09-11 00:28:55 +0530

    Added links in contact section and header

--- js/home.js
+++ js/home.js
@@ -21,4 +21,20 @@ $(document).ready(function() {
       remainingLineColor: 'rgba(200,200,200,0.1)'
     });
   })
+
+
+  $('nav a[href*=#], #home a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
 });
